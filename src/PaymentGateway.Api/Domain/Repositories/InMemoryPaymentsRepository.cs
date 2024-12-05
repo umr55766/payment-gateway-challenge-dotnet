327d0d82048edf8a6d9d3c7e905a1d5413106b2f
mohammadumair <umr55766@gmail.com>
2024-12-05 21:27:36 +0400

    Refactor InMemoryPaymentsRepository.cs

--- src/PaymentGateway.Api/Domain/Repositories/InMemoryPaymentsRepository.cs
+++ src/PaymentGateway.Api/Domain/Repositories/InMemoryPaymentsRepository.cs
@@ -1,18 +1,29 @@
-﻿using PaymentGateway.Api.Domain.Models.Responses;
+﻿using System.Collections.Concurrent;
+
+using PaymentGateway.Api.Domain.Aggregate;
+using PaymentGateway.Api.Domain.Exceptions;
 
 namespace PaymentGateway.Api.Domain.Repositories;
 
 public class InMemoryPaymentsRepository
 {
-    public List<PostPaymentResponse> Payments = new();
-    
-    public void Add(PostPaymentResponse payment)
+    private readonly ConcurrentDictionary<Guid, Payment> _payments = new();
+
+    public void Add(Payment payment)
     {
-        Payments.Add(payment);
+        if (!_payments.TryAdd(payment.Id, payment))
+        {
+            throw new PaymentAlreadyExistsException(payment.Id);
+        }
     }
 
-    public PostPaymentResponse Get(Guid id)
+    public Payment GetById(Guid id)
     {
-        return Payments.FirstOrDefault(p => p.Id == id);
+        if (!_payments.TryGetValue(id, out var payment))
+        {
+            throw new PaymentNotFoundException(id);
+        }
+
+        return payment;
     }
 }
