f02e7c9c677597c7d0dbd2ea6a6c223e70b55b68
mohammadumair <umr55766@gmail.com>
2024-12-06 17:17:41 +0400

    Annotate BankRequest and Response

--- src/PaymentGateway.Api/Domain/Models/Requests/BankRequest.cs
+++ src/PaymentGateway.Api/Domain/Models/Requests/BankRequest.cs
@@ -1,10 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace PaymentGateway.Api.Domain.Models.Requests;
 
 public class BankRequest
 {
+    [JsonPropertyName("card_number")]
     public string CardNumber { get; set; }
+    
+    [JsonPropertyName("expiry_date")]
     public string ExpiryDate { get; set; }
+    
+    [JsonPropertyName("currency")]
     public string Currency { get; set; }
+    
+    [JsonPropertyName("amount")]
     public int Amount { get; set; }
+    
+    [JsonPropertyName("cvv")]
     public string Cvv { get; set; }
 }
--- src/PaymentGateway.Api/Domain/Models/Responses/BankResponse.cs
+++ src/PaymentGateway.Api/Domain/Models/Responses/BankResponse.cs
@@ -1,7 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace PaymentGateway.Api.Domain.Models.Responses;
 
 public class BankResponse
 {
+    [JsonPropertyName("authorized")]
     public bool Authorized { get; set; }
+    
+    [JsonPropertyName("authorization_code")]
     public string AuthorizationCode { get; set; }
 }
