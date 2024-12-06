2a5fbaf032c8b6d070d60ca2a22b92cd1c2deb20
mohammadumair <umr55766@gmail.com>
2024-12-06 17:20:24 +0400

    Tie up ends in Program.cs

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,15 +1,26 @@
+using PaymentGateway.Api.Domain.HttpClients;
 using PaymentGateway.Api.Domain.Repositories;
+using PaymentGateway.Api.Domain.Services;
+using PaymentGateway.Api.Domain.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<InMemoryPaymentsRepository>();
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddSingleton<IBankClient, BankHttpClient>();
+builder.Services.AddScoped<PaymentService>();
+
+builder.Services.Configure<BankClientSettings>(builder.Configuration.GetSection("BankClient"));
+
 
 var app = builder.Build();
 
