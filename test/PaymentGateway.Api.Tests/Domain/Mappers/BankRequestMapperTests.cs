667374ee6a29876d6f3e7f834577fca34bdc36c4
mohammadumair <umr55766@gmail.com>
2024-12-06 15:12:46 +0400

    Add BankRequestMapperTest

---
+++ test/PaymentGateway.Api.Tests/Domain/Mappers/BankRequestMapperTests.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+
+using PaymentGateway.Api.Domain.Aggregate;
+using PaymentGateway.Api.Domain.Entities;
+using PaymentGateway.Api.Domain.Enums;
+using PaymentGateway.Api.Domain.Mappers;
+
+namespace PaymentGateway.Api.Tests.Domain.Mappers;
+
+public class BankRequestMapperTests
+{
+    [Fact]
+    public void MapToBankRequest_ShouldMapPaymentToBankRequest_WhenPaymentIsValid()
+    {
+        var payment = new Payment(
+            Guid.NewGuid(),
+            PaymentStatus.Pending,
+            new Money(100, "USD", 2),
+            new Card("1234567890123456", 12, 2025, "123")
+        );
+
+        var result = BankRequestMapper.MapToBankRequest(payment);
+
+        result.Should().NotBeNull();
+        result.CardNumber.Should().Be("1234567890123456");
+        result.ExpiryDate.Should().Be("12/2025");
+        result.Currency.Should().Be("USD");
+        result.Amount.Should().Be(100);
+        result.Cvv.Should().Be("123");
+    }
+
+    [Fact]
+    public void MapToBankRequest_ShouldThrowException_WhenPaymentIsNull()
+    {
+        var act = () => BankRequestMapper.MapToBankRequest(null);
+        act.Should().Throw<ArgumentNullException>().WithMessage("Payment cannot be null. (Parameter 'payment')");
+    }
+}
