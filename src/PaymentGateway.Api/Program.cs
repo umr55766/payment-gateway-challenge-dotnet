a378bda960eedfba9902346d70060bf547d51926
mohammadumair <umr55766@gmail.com>
2024-12-09 11:05:02 +0400

    Enable Swagger

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -68,12 +68,8 @@ if (otlpEndpoint != null)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
