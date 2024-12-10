1b5f583ce324738fe71639271f2697ecdaa38c89
mohammadumair <umr55766@gmail.com>
2024-12-10 17:47:48 +0400

    Refactor load test

--- test/PaymentGateway.Api.Tests/load/load_test.js
+++ test/PaymentGateway.Api.Tests/load/load_test.js
@@ -1,7 +1,6 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
-// Configuration for load testing
 export const options = {
     stages: [
         { duration: '30s', target: 10 },  // Ramp up to 10 users in 30 seconds
@@ -10,11 +9,24 @@ export const options = {
     ],
 };
 
+function assert() {
+    return {
+        'status is 200': (r) => r.status === 200,
+        'response contains id': (r) => JSON.parse(r.body).hasOwnProperty("id"),
+        'payment should be authorized': (r) => JSON.parse(r.body).status === "Authorized",
+        'response time is below 500ms': (r) => r.timings.duration < 500,
+    };
+}
+
+const params = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
 export default function () {
-    // Define the endpoint to test
     const url = 'http://localhost:5001/api/payments';
 
-    // Create a sample payload for testing
     const payload = JSON.stringify({
         "cardNumber": "2222405343248877",
         "expiryMonth": "04",
@@ -23,25 +35,8 @@ export default function () {
         "amount": 100,
         "cvv": "123"
     });
-
-    // Define headers
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    // Send a POST request
+    
     const response = http.post(url, payload, params);
-
-    // Validate response with checks
-    check(response, {
-        'status is 200': (r) => r.status === 200,
-        'response contains id': (r) => JSON.parse(r.body).hasOwnProperty("id"),
-        'payment should be authorized': (r) => JSON.parse(r.body).status === "Authorized",
-        'response time is below 500ms': (r) => r.timings.duration < 500,
-    });
-
-    // Simulate a small wait between requests
-    sleep(1);
+    
+    check(response, assert());
 }
